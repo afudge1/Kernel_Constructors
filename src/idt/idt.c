44c1a1009dbb68657ecaa865a4037ced8e51eeb1
afudge1 <166938653+afudge1@users.noreply.github.com>
2024-11-02 14:29:17 -0700

    stuff

---
+++ src/idt/idt.c
@@ -0,0 +1,23 @@
+#include "idt.h"
+
+struct idt_desc idt_descriptors[total_interrupts];
+struct idtr_desc idtr_descriptor;
+
+
+void idt_init() {
+    memset(idt_descriptors, 0, sizeof(idt_descriptors));
+    idtr_descriptor.limit = sizeof(idt_descriptors) - 1;
+    idtr_descriptor.base = (uint32_t)idt_descriptors;
+
+    for(int i = 0; i < total_interrupts; i++) {
+        idt_set(i, interrupt_pointer_table[i]);
+    }
+    idt_set(0, idt_zero);
+
+    // Load the interrupt descriptor table
+    idt_load(&idtr_descriptor);
+}
+
+idt_zero() {
+    printf("Divide by zero error");
+}
