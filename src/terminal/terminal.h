16bd0477aa7678c1b29277bca4d963e75b31b72b
QuanD <42784039+KevinDo76@users.noreply.github.com>
2024-11-02 16:52:17 -0700

    Revert "bruh"
    This reverts commit 6dfdc07d7e655c6508e657ad93eb3636b617f4cd.

--- src/terminal/terminal.h
+++ src/terminal/terminal.h
@@ -6,12 +6,15 @@
 
 //char** terminal_text_history = (char* )malloc(sizeof(char*) * MAX_TEMRINAL_HISTORY);
 
-extern char cwd[MAX_TERMINAL_PREFIX_BUFF+1];
-extern char terminal_buff[(MAX_TEMRINAL_HISTORY+1)* VGA_WIDTH];
-extern char terminal_input_buff[MAX_INPUT_BUFFER+1];
-extern int terminal_active;
-extern uint32_t textbuffSize;
-
 void terminal_init();
 int terminal_input(const char* text);
+void terminal_enter();
+void terminal_backspace();
+
+void keyboard_input_handler(char c);
+
+void terminal_pause_render();
+void terminal_resume_render();
+
+void terminal_reload();
 #endif
--- src/terminal/vga_text_graphic.h
+++ src/terminal/vga_text_graphic.h
@@ -1,14 +1,20 @@
-#ifndef VGA_TEXT_GRAPHIC_H
-#define VGA_TEXT_GRAPHIC_H
+#ifndef VGA_TEXT_GRAPHIC
+#define VGA_TEXT_GRAPHIC
 #include <stdint.h>
 
-extern uint16_t* video_memory;
-extern uint16_t color_byte;
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 20
+#define VGA_ADDRESS 0xB8000
+#define VGA_TEXT_WHITE_DEFAULT 0b00001111
 
-void vga_init();
+void vga_setForeground(char color);
+void vga_setBackground(char color);
+void vga_set(int32_t x, int32_t  y, const char* text);
+void vga_fill(int32_t y, int32_t  x, int32_t  width, int32_t  height, char text);
+void vga_copy(int32_t x, int32_t  y, int32_t  width, int32_t  height, uint32_t tx, uint32_t ty);
+char vga_get_char(int32_t x, int32_t y); 
 void vga_put_char(int32_t x, int32_t y, char c);
-void vga_set(int32_t  x, int32_t  y, const char* text);
-void vga_fill(int32_t  y, int32_t  x, int32_t  width, int32_t  height, char text);
-void vga_copy(int32_t  x, int32_t  y, int32_t  width, int32_t  height, uint32_t tx, uint32_t ty);
-char vga_get_char(int32_t x, int32_t y);
+void vga_init();
+
+
 #endif
