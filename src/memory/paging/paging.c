caa9fa2667a7d8b3e240d598e541d6d6dc7ffa15
Caleb Stewart <cstewart15@ewu.edu>
2024-11-02 15:31:40 -0700

    Comments

--- src/memory/paging/paging.c
+++ src/memory/paging/paging.c
@@ -1,34 +1,46 @@
 #include "paging.h"
 #include "../heap/kheap.h"
 
-// https://wiki.osdev.org/Paging
-
+// Load the given page directory into CR3 register
 void paging_load_directory(uint32_t directory);
-static uint32_t* current_directory = 0;
+static uint32_t* current_directory = 0; // Pointer to the current paging directory
 
+// Create a new 4GB paging chunk with specified flags
 struct paging_4gb_chunk* paging_new_4gb(uint8_t flags) {
-    uint32_t* directory = kzalloc(sizeof(uint32_t) * PAGING_TOTAL_ENTRIES_PER_TABLE);
+    // Allocate directory
+    uint32_t* directory = kzalloc(sizeof(uint32_t) * PAGING_TOTAL_ENTRIES_PER_TABLE); 
 
     int offset = 0;
-    for(int i = 0; i < PAGING_TOTAL_ENTRIES_PER_TABLE; ++i) {
-        uint32_t* entry = kzalloc(sizeof(uint32_t) * PAGING_TOTAL_ENTRIES_PER_TABLE);
-        for(int j = 0; j < PAGING_TOTAL_ENTRIES_PER_TABLE; ++j) {
-            entry[j] = (offset + (j * PAGING_PAGE_SIZE)) | flags;
+    for (int i = 0; i < PAGING_TOTAL_ENTRIES_PER_TABLE; ++i) {
+        // Allocate page table entry
+        uint32_t* entry = kzalloc(sizeof(uint32_t) * PAGING_TOTAL_ENTRIES_PER_TABLE); 
+        for (int j = 0; j < PAGING_TOTAL_ENTRIES_PER_TABLE; ++j) {
+            // Set up page table entries
+            entry[j] = (offset + (j * PAGING_PAGE_SIZE)) | flags; 
         }
-        offset += (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE);
-        directory[i] = (uint32_t)entry | flags | PAGING_IS_WRITEABLE;
+        // Update offset for next table
+        offset += (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE); 
+        // Link page table to directory
+        directory[i] = (uint32_t)entry | flags | PAGING_IS_WRITEABLE; 
     }
-
-    struct paging_4gb_chunk* chunk_4gb = kzalloc(sizeof(struct paging_4gb_chunk));
-    chunk_4gb->directory_entry = directory;
-    return chunk_4gb;
+    // Create 4GB chunk
+    struct paging_4gb_chunk* chunk_4gb = kzalloc(sizeof(struct paging_4gb_chunk)); 
+    // Assign the directory entry
+    chunk_4gb->directory_entry = directory; 
+    // Return the new chunk
+    return chunk_4gb; 
 }
 
+// Switch the current paging directory
 void paging_switch(uint32_t* directory) {
-    paging_load_directory(directory);
-    current_directory = directory;
+    // Load the new directory
+    paging_load_directory(directory); 
+    // Update the current directory pointer
+    current_directory = directory; 
 }
 
+// Get the directory of the 4GB chunk
 uint32_t* paging_4gb_chunk_get_directory(struct paging_4gb_chunk* chunk) {
-    return chunk->directory_entry;
+    // Return the directory entry of the chunk
+    return chunk->directory_entry; 
 }
--- src/memory/paging/paging.h
+++ src/memory/paging/paging.h
@@ -3,22 +3,26 @@
 #include <stdint.h>
 #include <stddef.h>
 
+// Paging flags
 #define PAGING_CACHE_DISABLED 0x10
 #define PAGING_WRITE_THROUGH 0x8
 #define PAGING_ACCESS_FROM_ALL 0x4
 #define PAGING_IS_WRITEABLE 0x2
 #define PAGING_IS_PRESENT 0x1
 
+// Paging configuration
 #define PAGING_TOTAL_ENTRIES_PER_TABLE 1024
 #define PAGING_PAGE_SIZE 4096
+
+// Structure to hold a 4GB paging chunk
 struct paging_4gb_chunk {
-    uint32_t* directory_entry;
+    uint32_t* directory_entry; // Pointer to the page directory
 };
 
+// Function declarations
 struct paging_4gb_chunk* paging_new_4gb(uint8_t flags);
 void paging_switch(uint32_t* directory);
-void enable_paging(); // From assembly
+void enable_paging(); // Assembly function to enable paging
 uint32_t* paging_4gb_chunk_get_directory(struct paging_4gb_chunk* chunk);
-
 
 #endif
