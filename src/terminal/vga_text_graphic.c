019d7b0ef1a63e7686b4e0c6c9c719af74ed3051
QuanD <42784039+KevinDo76@users.noreply.github.com>
2024-11-02 16:54:21 -0700

    Reapply "bruh"
    This reverts commit 16bd0477aa7678c1b29277bca4d963e75b31b72b.

--- src/terminal/vga_text_graphic.c
+++ src/terminal/vga_text_graphic.c
@@ -1,26 +1,17 @@
-//#include "libc/libc.h"
-#include <stddef.h>
 #include "vga_text_graphic.h"
+#include <stdint.h> 
+#include "libc/string.h"
+#include "terminal_config.h"
 
-uint8_t color_byte;
 uint16_t* video_memory;
+uint16_t color_byte;
 
 void vga_init()
 {
+    video_memory = (uint16_t*)VGA_ADDRESS;
     color_byte = VGA_TEXT_WHITE_DEFAULT;
-    video_memory = (uint16_t*)(VGA_ADDRESS);
+    vga_fill(0,0,VGA_WIDTH,VGA_HEIGHT,' ');
 }
-
-void vga_setForeground(char color)
-{
-    
-}
-
-void vga_setBackground(char color)
-{
-    color_byte = (color_byte & 0b11110000) | (color & 0b00001111);
-}
-
 void vga_set(int32_t  x, int32_t  y, const char* text)
 {
     uint32_t length = strlen(text);
@@ -29,6 +20,18 @@ void vga_set(int32_t  x, int32_t  y, const char* text)
         vga_put_char(x+i,y,text[i]);
     }
 }
+
+void vga_put_char(int32_t x, int32_t y, char c)
+{
+    if (x < 0 || x >= VGA_WIDTH || y < 0 || y >= VGA_HEIGHT)
+    {
+        return;
+    }
+
+    uint32_t position = (y * VGA_WIDTH) + x;
+    video_memory[position] = (uint16_t)c | (color_byte << 8);
+}
+
 void vga_fill(int32_t  y, int32_t  x, int32_t  width, int32_t  height, char text)
 {
     for (int y=0;y<height;y++)
@@ -39,6 +42,7 @@ void vga_fill(int32_t  y, int32_t  x, int32_t  width, int32_t  height, char text
         }
     }
 }
+
 void vga_copy(int32_t  x, int32_t  y, int32_t  width, int32_t  height, uint32_t tx, uint32_t ty)
 {
     for (int y=0;y<height;y++)
@@ -58,19 +62,5 @@ char vga_get_char(int32_t x, int32_t y)
     }
 
     uint32_t position = (y * VGA_WIDTH) + x;
-    uint16_t* vga_write_location = (uint16_t*)((uint32_t)video_memory + (uint32_t)position);
-
-    return *(vga_write_location) >> 8;
-}
-
-void vga_put_char(int32_t x, int32_t y, char c)
-{
-    if (x < 0 || x >= VGA_WIDTH || y < 0 || y >= VGA_HEIGHT)
-    {
-        return;
-    }
-
-    uint32_t position = (y * VGA_WIDTH) + x;
-    uint16_t* vga_write_location = (uint16_t*)((uint32_t)video_memory + (uint32_t)position);
-    *vga_write_location = (uint16_t)color_byte | (c << 8);
+    return video_memory[position] & 0xFF;
 }
